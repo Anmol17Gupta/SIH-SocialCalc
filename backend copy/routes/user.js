5691f933018d67c838bdc5b8ae931b7f70ccba5c
saakshatjain <123252511+saakshatjain@users.noreply.github.com>
2024-09-01 02:07:19 +0530

    added database

---
+++ backend copy/routes/user.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const zod = require("zod");
+const { User } = require("../db");
+
+const signupBody = zod.object({
+    username: zod.string().email(),
+	firstname: zod.string(),
+	lastname: zod.string(),
+	password: zod.string()
+})
+
+router.post("/signup", async (req, res) => {
+    const { success } = signupBody.safeParse(req.body)
+    if (!success) {
+        console.log(req.body.username + req.body.firstname + req.body.lastname + req.body.password);
+        return res.status(400).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const existingUser = await User.findOne({
+        username: req.body.username
+    })
+
+    if (existingUser) {
+        return res.status(411).json({
+            message: "Email already taken"
+        })
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+    })
+    const userId = user._id;
+    res.json({
+        message: "User created successfully",
+    })
+})
+
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+	password: zod.string()
+})
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user) {
+        res.json({
+           msg : "successful",
+        })
+        return;
+    }
+
+    
+    res.status(411).json({
+        message: "No such user exists"
+    })
+})
+
+module.exports = router;
